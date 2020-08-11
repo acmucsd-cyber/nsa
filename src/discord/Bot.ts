2801994b37f856d5717b1b44d24205678e939bf4
agop2020 <67121761+agop2020@users.noreply.github.com>
2020-08-11 11:22:42 -0700

    Update Bot.ts

--- src/discord/Bot.ts
+++ src/discord/Bot.ts
@@ -32,6 +32,42 @@ export default class Bot {
 			// process commands here
 			
 		}
+		//Ping Test
+		this.client.on("ready", () =>
+		{
+			console.log('Logged in as ${client.user.tag}!')
+		})
+	
+		this.client.on("message", msg =>
+		{
+			if (msg.content === "ping")
+			{
+				console.log("got a ping")
+				msg.reply("Pong!")
+			}
+		})
+		//Sending new users a welcome DM
+		this.client.on('guildMemberAdd', member =>
+		{
+			console.log("new member")
+			member.send("Welcome to the ACM Cyber server! Get your roles in #roles. Once you introduce yourself in #introductions, you'll have access to the full ACM Cyber Server!")
+				.catch(console.error);
+		})
+		//Giving users server access once they type in the introductions channel. Need to give the bot "manage roles" perms.
+		this.client.on("message", msg =>
+		{
+			if(msg.channel.id === '742797435520417828') //ID of introductions channel
+			{
+				if (msg.content.includes(' '))
+				{
+					if(!(msg.member.roles.cache.has('742797850630684762'))) //ID of "verified" role
+					{
+						console.log("addrole")
+						msg.member.roles.add('742797850630684762')
+					}
+				}
+			}
+		})
 	}
 
 	public connect(): void {
