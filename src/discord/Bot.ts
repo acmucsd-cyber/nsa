abcbe8a4f6f76dba1c047885d8edc2104a32e334
agop2020 <67121761+agop2020@users.noreply.github.com>
2020-08-11 20:44:18 -0700

    Update Bot.ts

--- src/discord/Bot.ts
+++ src/discord/Bot.ts
@@ -7,6 +7,37 @@ export default class Bot {
 		private readonly client: Client = new Discord.Client()
 	) {
 		this.client.on('message', (msg: Message) => this.messageHandler.bind(this)(msg))
+		//Ping Test
+		this.client.on("message", msg =>
+        	{
+            		if (msg.content === "ping")
+            		{		
+                		console.log("got a ping")
+                		msg.reply("Pong!")
+            		}
+        	})
+        	//Sending new users a welcome DM
+        	this.client.on('guildMemberAdd', member =>
+        	{
+            		console.log("new member")
+            		member.send("Welcome to ACM Cyber's Discord! ACM Cyber is an ACM sub-org and community of all kinds of cybersecurity enthusiasts and hobbyists. This Discord server is our virtual home and a hub for our members -- it's where we will announce club activities and events, hold spontaneous discussions, post memes, and more! We welcome everyone from all skill levels and backgrounds. Read the rules in #rulesandinfo and once you introduce yourself in #introductions, you'll have access to the full server! Get your roles over in #roles, and join the discussion in #lobby!")
+                		.catch(console.error);
+        	})
+        	//Giving users server access once they type in the introductions channel. Need to give the bot "manage roles" perms.
+        	this.client.on("message", msg =>
+        	{
+            		if(msg.channel.id === '742797435520417828') //ID of introductions channel
+            		{
+                		if (msg.content.includes(' '))
+                		{
+                    			if(!(msg.member.roles.cache.has('742797850630684762'))) //ID of "member" role
+                    			{
+                        			console.log("addrole")
+                        			msg.member.roles.add('742797850630684762')
+                    			}
+                		}
+            		}
+        	})
 	}
 
 	public logInfo(info: string): void {
@@ -30,45 +61,7 @@ export default class Bot {
 			const commandString: string[] = msg.content.substr(prefix.length).trim().split(' ')
 			
 			// process commands here
-			
 		}
-		//Ping Test
-		this.client.on("ready", () =>
-		{
-			console.log('Logged in as ${client.user.tag}!')
-		})
-	
-		this.client.on("message", msg =>
-		{
-			if (msg.content === "ping")
-			{
-				console.log("got a ping")
-				msg.reply("Pong!")
-			}
-		})
-		//Sending new users a welcome DM
-		this.client.on('guildMemberAdd', member =>
-		{
-			console.log("new member")
-			member.send("Welcome to ACM Cyber's Discord! ACM Cyber is an ACM sub-org and community of all kinds of cybersecurity enthusiasts and hobbyists. This Discord server is our virtual home and a hub for our members -- it's where we will announce club activities and events, hold spontaneous discussions, post memes, and more! We welcome everyone from all skill levels and backgrounds. Read the rules in #rulesandinfo and once you introduce yourself in #introductions, you'll have access to the full server! Get your roles over in #roles, and join the discussion in #lobby!")
-				.catch(console.error);
-
-		})
-		//Giving users server access once they type in the introductions channel. Need to give the bot "manage roles" perms.
-		this.client.on("message", msg =>
-		{
-			if(msg.channel.id === '742797435520417828') //ID of introductions channel
-			{
-				if (msg.content.includes(' '))
-				{
-					if(!(msg.member.roles.cache.has('742797850630684762'))) //ID of "member" role
-					{
-						console.log("addrole")
-						msg.member.roles.add('742797850630684762')
-					}
-				}
-			}
-		})
 	}
 
 	public connect(): void {
